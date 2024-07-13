f1ef9d034764ebb30025fbc2021069a3dc68ee98
Rony Moura <ronymmoura@gmail.com>
2024-07-13 16:49:34 -0300

    feat: add Button

---
+++ packages/web-ui/src/components/Button/ButtonRoot.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { type VariantProps } from "class-variance-authority";
+import { FaSpinner } from "react-icons/fa6";
+
+import { buttonVariants } from "./ButtonVariants";
+import { cn } from "../../util/cn";
+import { Button } from ".";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  isLoading?: boolean;
+}
+
+const ButtonRoot = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ variant, className, isLoading, type = "button", children, size, onClick, ...props }, ref) => {
+    return (
+      <button
+        className={cn(buttonVariants({ variant, size, className }))}
+        type={type}
+        ref={ref}
+        onClick={(e) => {
+          typeof onClick !== "undefined" && !isLoading && onClick(e);
+        }}
+        {...props}
+      >
+        {isLoading && (
+          <>
+            <Button.Icon>
+              <FaSpinner className="animate-spin" />
+            </Button.Icon>
+          </>
+        )}
+        {!isLoading && children}
+      </button>
+    );
+  },
+);
+
+ButtonRoot.displayName = "ButtonRoot";
+
+export { ButtonRoot };
---
+++ packages/web-ui/types/components/Button/ButtonVariants.d.ts
@@ -0,0 +1,4 @@
+export declare const buttonVariants: (props?: {
+    variant?: "default" | "accent" | "success" | "danger" | "ghost";
+    size?: "default" | "sm" | "lg" | "icon";
+} & import("class-variance-authority/types").ClassProp) => string;
