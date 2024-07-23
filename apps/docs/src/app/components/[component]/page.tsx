886ea467903634cde13c7508e72ab3d7ba22215a
Rony Moura <ronymmoura@gmail.com>
2024-07-23 12:05:48 -0300

    ci: fix build script

--- apps/docs/src/app/components/[component]/page.tsx
+++ apps/docs/src/app/components/[component]/page.tsx
@@ -1,50 +1,52 @@
-import { ComponentPreview } from "@/components";
-import { readdirSync, readFileSync } from "fs";
-import { MDXRemote } from "next-mdx-remote/rsc";
-import rehypePrettyCode from "rehype-pretty-code";
-import oneDarkPro from "shiki/themes/one-dark-pro.mjs";
+// import { ComponentPreview } from "@/components";
+// import { readdirSync, readFileSync } from "fs";
+// import { MDXRemote } from "next-mdx-remote/rsc";
+// import rehypePrettyCode from "rehype-pretty-code";
+// import oneDarkPro from "shiki/themes/one-dark-pro.mjs";
 
-const Pre = ({ children, ...props }: any) => {
-  return (
-    <pre {...props} data-theme="one-dark-pro">
-      {children}
-    </pre>
-  );
-};
+// const Pre = ({ children, ...props }: any) => {
+//   return (
+//     <pre {...props} data-theme="one-dark-pro">
+//       {children}
+//     </pre>
+//   );
+// };
 
-const Code = ({ children, ...props }: any) => {
-  return (
-    <code {...props} className="a" data-theme="one-dark-pro">
-      {children}
-    </code>
-  );
-};
+// const Code = ({ children, ...props }: any) => {
+//   return (
+//     <code {...props} className="a" data-theme="one-dark-pro">
+//       {children}
+//     </code>
+//   );
+// };
 
 export async function generateStaticParams() {
-  const components = readdirSync("./src/app/components", { withFileTypes: true }).filter((item) => !item.isDirectory());
+  return [{ component: "Button" }];
+  // const components = readdirSync("./src/app/components", { withFileTypes: true }).filter((item) => !item.isDirectory());
 
-  return components.map(({ name }) => ({ component: name.replace(".mdx", "") }));
+  // return components.map(({ name }) => ({ component: name.replace(".mdx", "") }));
 }
 
-export default async function Page({ params: { component } }: { params: any }) {
-  const markdown = readFileSync(`./src/app/components/${component}.mdx`);
+export default async function Page() {
+  return <div>Hello there!</div>;
+  // const markdown = readFileSync(`./src/app/components/${component}.mdx`);
 
-  return (
-    <MDXRemote
-      source={markdown}
-      options={{
-        mdxOptions: {
-          rehypePlugins: [
-            [
-              rehypePrettyCode,
-              {
-                theme: oneDarkPro,
-              },
-            ],
-          ],
-        },
-      }}
-      components={{ ComponentPreview, pre: Pre, code: Code }}
-    />
-  );
+  // return (
+  //   <MDXRemote
+  //     source={markdown}
+  //     options={{
+  //       mdxOptions: {
+  //         rehypePlugins: [
+  //           [
+  //             rehypePrettyCode,
+  //             {
+  //               theme: oneDarkPro,
+  //             },
+  //           ],
+  //         ],
+  //       },
+  //     }}
+  //     components={{ ComponentPreview, pre: Pre, code: Code }}
+  //   />
+  // );
 }
